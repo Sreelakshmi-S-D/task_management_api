961f335013a248ab8cd8eb7c201db9799059291e
Sreelakshmi-S-D <sreelakshmisdsreelakam@gmail.com>
2026-01-10 10:45:07 +0530

    new changes

--- tasks/models.py
+++ tasks/models.py
@@ -2,16 +2,26 @@ from django.db import models
 from .constants import STATUS_CHOICES,STATUS_PENDING
 
 # Create your models here.
-class Task(models.Model):
-    title = models.CharField('Title',max_length=255,)
+
+class Status(models.Model):
     status = models.CharField(
         'Status',
         max_length=10,
         choices=STATUS_CHOICES,
         default=STATUS_PENDING
     )
+class Task(models.Model):
+    title = models.CharField('Title',max_length=255)
+    status = models.ForeignKey(
+        Status,
+        on_delete=models.CASCADE,
+        default=1,
+        related_name='tasks'
+    )
     created_at = models.DateTimeField('Created At',auto_now_add=True)
     updated_at = models.DateTimeField('Updated At',auto_now=True)
 
     def __str__(self):
         return self.title
+    
+
